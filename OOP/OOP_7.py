0efc97198545dc3e460b93682e40b594779bc83f
Kirill <fkirill69@gmail.com>
2024-11-05 19:38:22 +0300

    Commit2
    pick 84d5ee5 finally
    
    - Please enter the commit message for your changes. Lines starting
    wq

--- OOP/OOP_7.py
+++ OOP/OOP_7.py
@@ -1,9 +1,29 @@
 class Person:
-    def __init__(self, name, surname):
-        self._name = name
-        self._surname = surname
-        self.name = f'{self._name} {self._surname}'
+    def create(self):
+        self.name = 'Ivan'
+
+    def display(self):
+        print(self.name)
 
 
-p = Person('John', 'Smith')
-print(p.name)  # -> 'John Smith'
+p = Person()
+# p.display()  # -> AttributeError: 'Person' object has no attribute 'name'
+
+p.create()  # -> Создали экземпляру атрибут name
+p.display()  # -> 'Ivan'
+
+
+class Person:
+    def __init__(self, name):  # Метод init вызывается автоматически при вызове класса
+        self.name = name
+
+    def display(self):
+        print(self.name)
+
+
+p = Person(name='Ivan')  # Вызываем класс и передаём в метод __init__ аргумент
+p.display()  # -> 'Ivan'
+
+# Создание экземпляров классов состоит из 2 этапов
+# 1. Когда вызываем класс, Python методом __new__ создаёт экземпляр класса
+# 2. Метод __init__ присваивает созданному ранее экземпляру свойства на лету
--- OOP/OOP_9.py
+++ OOP/OOP_9.py
@@ -1,4 +1,4 @@
-name = 'Kirill'
+name = 'Ivan'
 
 
 class Person:
@@ -7,5 +7,67 @@ class Person:
     def print_name(self):
         print(name)
 
+    def print_name_self(self):
+        print(self.name)
+
+    @classmethod
+    def change_name(cls, name):
+        cls.name = name
+
+
 p = Person()
-p.print_name()
+
+p.print_name()  # -> 'Ivan'
+p.print_name_self()  # -> 'Dima'
+
+# Область видимости методов экземпляров не является вложенной в область видимости класса
+
+p.name = 'Kirill'
+print(p.__dict__)  # -> {'name': 'Kirill'} - Изменилось только свойство экземпляра
+print(Person.name)  # -> 'Dima'
+
+p.print_name_self()  # -> 'Kirill'
+
+# Из экземпляра мы можем только читать свойства класса, но не менять их. @classmethod позволяет сделать это
+
+p.change_name('Kirill')
+print(Person.name)  # -> 'Kirill'
+
+
+# Самый известный прием для использования методов классов - альтернативные __init__ методы
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def from_file(cls, path):
+        with open(path) as f:
+            name = f.read().strip()
+        return cls(name=name)
+
+    @classmethod
+    def from_obj(cls, obj):
+        if hasattr(obj, 'name'):
+            name = getattr(obj, 'name')
+            return cls(name=name)
+        return cls
+
+
+p = Person('Kirill')
+
+print(p.__dict__)  # -> {'name': 'Kirill'}
+
+p = Person.from_file('text')
+
+
+print(p.__dict__)  # -> {'name': 'Alena'}
+
+
+class Config:
+    name = 'Igor'
+
+
+p = Person.from_obj(Config)
+
+print(p.__dict__)  # -> {'name': 'Igor'}
