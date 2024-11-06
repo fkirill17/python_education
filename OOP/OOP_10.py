cc3912fa7c23428721486f6364c9572c4ad285ff
Kirill <fkirill69@gmail.com>
2024-11-06 17:05:43 +0300

    Commit3

--- OOP/OOP_10.py
+++ OOP/OOP_10.py
@@ -1,1 +1,26 @@
+class Person:
+    def __init__(self, name):
+        self._name = name
 
+    @property
+    def name(self):
+        print('Код геттера')
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        print('Код сеттера')
+        self._name = value
+
+# Названия декораторов и функций строго определено
+
+
+p = Person('Dima')
+
+print(p.name)  # -> 'Код геттера' -> 'Dima'
+
+p.name = 'Kirill'  # -> 'Код сеттера'
+print(p.name)  # -> 'Код геттера' -> 'Kirill'
+
+# Геттеры и сеттеры можно использовать как буферную зону, для полученя или редактирования атрибутов экземпляра класса
+# Их так же можно применять для валидации значений, которые мы собираемся записать в атрибутах
