4547026a404f468777931f744f640df3d3138788
Kirill <fkirill69@gmail.com>
2025-02-15 21:29:01 +0300

    changed oop7

--- OOP/OOP_7.py
+++ OOP/OOP_7.py
@@ -1,29 +1,12 @@
-class Person:
-    def create(self):
-        self.name = 'Ivan'
-
-    def display(self):
-        print(self.name)
-
-
-p = Person()
-# p.display()  # -> AttributeError: 'Person' object has no attribute 'name'
-
-p.create()  # -> Создали экземпляру атрибут name
-p.display()  # -> 'Ivan'
-
+# Пример приватных атрибутов
 
 class Person:
-    def __init__(self, name):  # Метод init вызывается автоматически при вызове класса
-        self.name = name
+    def __init__(self, name, surname):
+        self._name = name
+        self._surname = surname
+        self.name = f'{self._name} {self._surname}'
 
-    def display(self):
-        print(self.name)
 
+p = Person('John', 'Smith')
 
-p = Person(name='Ivan')  # Вызываем класс и передаём в метод __init__ аргумент
-p.display()  # -> 'Ivan'
-
-# Создание экземпляров классов состоит из 2 этапов
-# 1. Когда вызываем класс, Python методом __new__ создаёт экземпляр класса
-# 2. Метод __init__ присваивает созданному ранее экземпляру свойства на лету
+print(p.name)  # -> 'John Smith'
