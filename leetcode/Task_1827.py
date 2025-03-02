e1b3ccf3eaf154e0311b0f3b862fbba9b37ba17b
Kirill <fkirill69@gmail.com>
2025-03-02 22:21:14 +0300

    commit-34

--- leetcode/Task_1827.py
+++ leetcode/Task_1827.py
@@ -1,9 +1,10 @@
 def minOperations(nums):
     x = 0
     for idx in range(len(nums) - 1):
-        while nums[idx] >= nums[idx + 1]:
-            nums[idx + 1] += 1
-            x += 1
+        if nums[idx] >= nums[idx + 1]:
+            diff = nums[idx] - nums[idx + 1] + 1
+            nums[idx + 1] += diff
+            x += diff
     return x
 
 
