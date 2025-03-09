f242e2543f389a4addef8aa0e8e97c90a2358e67
Kirill <fkirill69@gmail.com>
2025-03-09 14:47:39 +0300

    commit-45

--- leetcode/1.py
+++
@@ -1,6 +0,0 @@
-def func(name, age):
-
-    print(name, age)
-
-a = {'name': 'Kirill', 'age': 24}
-func(**a)
---
+++ leetcode/Task_58.py
@@ -0,0 +1,13 @@
+def lengthOfLastWord(s):
+    lst_1 = list(s)
+    lst_1 = lst_1[::-1]
+    lst_1.append(" ")
+    lst_2 = []
+    for symbol in lst_1:
+        if symbol != " ":
+            lst_2.append(symbol)
+        if lst_2 != [] and symbol == " ":
+            return len(lst_2)
+
+
+print(lengthOfLastWord("a"))
