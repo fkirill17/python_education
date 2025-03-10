909e4b175805142f8d3bc5d304cd2118c4aa7c8a
Kirill <fkirill69@gmail.com>
2025-03-10 15:03:03 +0300

    commit-46

---
+++ leetcode/Task_66.py
@@ -0,0 +1,15 @@
+def plusOne(digits: list) -> list:
+    digits = digits[::-1]
+    for idx in range(0, len(digits)):
+        summa = digits[idx] + 1
+        if summa > 9:
+            digits[idx] = 0
+        else:
+            digits[idx] += 1
+            return digits[::-1]
+    digits.append(1)
+    return digits[::-1]
+
+
+
+print(plusOne([9, 8]))
