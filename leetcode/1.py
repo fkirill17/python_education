66b1d817a7a1a98394a3898ba4befcb06762a156
Kirill <fkirill69@gmail.com>
2025-03-11 21:58:33 +0300

    commit-48

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,6 +1,16 @@
-def main():
-    pass
+def isPalindrome(x: int) -> bool:
+    if x < 9:
+        return True
+    lst_1 = list(str(x))
+    ln = len(lst_1)
+    ctr = ln // 2
+    if ln % 2 != 0:
+        lst_1.pop(ctr)
+    lst_2 = lst_1[:ctr]
+    lst_1 = lst_1[ctr:]
+    if lst_1 == lst_2[::-1]:
+        return True
+    return False
 
 
-if __name__ == '__main__':
-    main()
+print(isPalindrome(7))
---
+++ leetcode/Task_67!.py
@@ -0,0 +1,19 @@
+def addBinary(a: str, b: str) -> str:
+    result = []
+    carry = 0  # Перенос
+    i, j = len(a) - 1, len(b) - 1  # Индексы последних символов
+
+    while i >= 0 or j >= 0 or carry:
+        bit_a = int(a[i]) if i >= 0 else 0
+        bit_b = int(b[j]) if j >= 0 else 0
+
+        total = bit_a + bit_b + carry
+        result.append(str(total % 2))  # Добавляем в результат младший бит
+        carry = total // 2  # Обновляем перенос
+
+        i -= 1
+        j -= 1
+
+    return ''.join(result[::-1])
+
+addBinary("1010", "1011")
