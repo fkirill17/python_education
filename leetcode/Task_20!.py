694d11b7ddad3b5dbc8f390442762043ee6701fd
Kirill <fkirill69@gmail.com>
2025-03-11 22:01:00 +0300

    commit-49

---
+++ leetcode/Task_20!.py
@@ -0,0 +1,17 @@
+def isValid(self, s):
+    open_to_close = {
+        '}': '{',
+        ')': '(',
+        ']': '['
+    }
+    stack = []
+
+    for c in s:
+        if c in open_to_close:  # Открывающая скобка?
+            if not (stack and
+                    (stack.pop() == open_to_close[c])):  # Стек пуст? И последний элемент стека != значение из словаря?
+                return False
+        else:  # Закрывающая скобка
+            stack.append(c)  # Добавляем скобку в стек
+
+    return not stack  # Если нет стека неявно вернется True
