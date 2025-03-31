41df685443d253868c1b9746ffbd869defa786cb
Kirill <fkirill69@gmail.com>
2025-03-31 21:34:23 +0300

    commit-69

--- algorithms/leetcode best time complexity/20. Valid Parentheses.py
+++ algorithms/leetcode best time complexity/20. Valid Parentheses.py
@@ -4,12 +4,12 @@ def isValid(s):
         '}': '{',
         ']': '['
     }
-    if len(s) % 2 != 0:
+    if len(s) % 2 != 0 or s[0] in dct:  # Если нечетное количество элементов или первый элемент закрывающая скобка
         return False
     stack = []
     for i in s:
-        if i in dct and stack and stack[-1] == dct[i]:
-            stack.pop()
+        if i in dct and stack and stack[-1] == dct[i]:  # Если противоположная скобка лежит в последнем элементе стека
+            stack.pop()  # Работа со стеком происходит через последний элемент
         else:
-            stack.append(i)
+            stack.append(i)  # Работа со стеком
     return not stack
