1fd61b3c611423316711369bb3f79c26e11f1bf2
Kirill <fkirill69@gmail.com>
2025-04-10 14:46:49 +0300

    commit-82

--- algorithms/leetcode_best_time_complexity/13. Roman to Integer.py
+++ algorithms/leetcode_best_time_complexity/13. Roman to Integer.py
@@ -1,30 +1,23 @@
 def romanToInt(s):
-    dct = {"I": 1,
-           "V": 5,
-           "X": 10,
-           "L": 50,
-           "C": 100,
-           "D": 500,
-           "M": 1000}
-    s = list(s)
-    lst = []
-    lst_2 = []
-    for sym in s:
-        lst.append(dct[sym])
-    l, r = 0, 1
-    while r < len(lst):
-        if lst[l] < lst[r]:
-            num = lst[r] - lst[l]
-            lst_2.append(num)
-            l += 2
-            r += 2
+    roman_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    total = 0
+    prev_value = 0
+    for char in reversed(s):
+        value = roman_map[char]
+        if prev_value > value:
+            total -= value
         else:
-            lst_2.append(lst[l])
-            l += 1
-            r += 1
-    if l < len(lst):
-        lst_2.append(lst[-1])
-    return sum(lst_2)
+            total += value
+        prev_value = value
+    return total
 
 
-print(romanToInt("MDIX"))
+print(romanToInt('MCMXCI'))  # -> 1991
--- algorithms/leetcode_best_time_complexity/sandbox.py
+++ algorithms/leetcode_best_time_complexity/sandbox.py
@@ -1,30 +1,23 @@
-class MyHashSet:
+def romanToInt(s):
+    roman_map = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    total = 0
+    prev_value = 0
+    for char in reversed(s):
+        value = roman_map[char]
+        if prev_value > value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+    return total
 
-    def __init__(self):
-        self.hashset = [[] for _ in range(10000)]
 
-    def add(self, key):
-        idx = key % len(self.hashset)
-        if not self.hashset[idx]:
-            self.hashset[idx].append(key)
-
-
-    def remove(self, key):
-        idx = key % len(self.hashset)
-        if self.hashset[idx]:
-            self.hashset[idx].remove(key)
-
-    def contains(self, key):
-        idx = key % len(self.hashset)
-        return key in self.hashset[idx]
-
-
-obj = MyHashSet()
-obj.add(1)
-obj.add(2)
-obj.remove(1)
-param_3 = obj.contains(1)
-param_4 = obj.contains(2)
-
-print(param_3)  # -> False
-print(param_4)  # -> True
+print(romanToInt('MCMXCI'))  # -> 1991
