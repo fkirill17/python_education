a7ae3457a0dd92f58754241ac3ec333f94b1b16d
Kirill <fkirill69@gmail.com>
2025-04-15 15:49:22 +0300

    commit-87

--- algorithms/leetcode_best_time_complexity/643. Maximum Average Subarray I.py
+++ algorithms/leetcode_best_time_complexity/643. Maximum Average Subarray I.py
@@ -1,16 +1,14 @@
 def findMaxAverage(nums, k):
-    summ = 0
-    for i in nums[0:k]:  # Первоначальное вычисление суммы элементов, далее на его основе реализуем скользящее окно
-        summ += i
-    out = summ
     l, r = 0, k
+    total_sum = sum(nums[:k])  # Первоначальное вычисление суммы элементов, далее на его основе реализуем скользящее окно
+    max_sum = 0  # Ключевая формула скользящего окна (Sliding Window)
     while r < len(nums):
-        summ = summ + nums[r] - nums[l]  # Ключевая формула скользящего окна
-        if out < summ:  # Накопление максимума в summ
-            out = summ
-        l += 1
+        actual_sum = total_sum - nums[l] + nums[r]
+        if actual_sum > max_sum:  # Накопление максимума
+            max_sum = actual_sum
         r += 1
-    return out / k
+        l += 1
+    return max_sum / k
 
 
 print(findMaxAverage([1,12,-5,-6,50,3], 4))  # -> 12.75
