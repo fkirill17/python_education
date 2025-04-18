31a7b4c8b54924f59295719537852f81e376ddd0
Kirill <fkirill69@gmail.com>
2025-04-18 13:01:25 +0300

    commit-93

--- algorithms/leetcode_best_time_complexity/sandbox.py
+++ algorithms/leetcode_best_time_complexity/sandbox.py
@@ -1,18 +1,18 @@
-class MyIterator:
-    def __init__(self, iterable):
-        self.index = 0
-        self.iterable = iterable
+def numEquivDominoPairs(dominoes):
+    dct = {}
+    out = 0
+    for domino in dominoes:
+        x = (min(domino), max(domino))
+        if x in dct:
+            dct[x] += 1
+        else:
+            dct[x] = 1
+    for i in dct:
+        rep_cnt = dct[i]
+        if rep_cnt > 1:
+            out += rep_cnt * (rep_cnt - 1) // 2
+    return out
 
-    def __iter__(self):
-        return self
 
-    def __next__(self):
-        if self.index > len(self.iterable):
-            raise StopIteration
-        val = self.iterable[self.index]
-        self.index += 1
-        return val
-
-
-iterator = MyIterator([1,2,3,4,5])
-print(iterator.__next__())
+print(numEquivDominoPairs([[1,2],[2,1],[3,4],[5,6]]))  # -> 1
+print(numEquivDominoPairs([[1,2],[1,2],[1,1],[1,2],[2,2]]))  # -> 3
