39d189866cc33d4323229c1efdd15acead159199
Kirill <fkirill69@gmail.com>
2025-06-14 16:01:30 +0300

    commit - 110

--- algorithms/leetcode_best_time_complexity/daily_tasks.py
+++ algorithms/leetcode_best_time_complexity/daily_tasks.py
@@ -1,8 +1,13 @@
 import random
 
-CNT_TASKS = 3
+CNT = 3
 
-SOL_CNT = 34
+CNT_LEETCODE = 35
 
-for i in range(CNT_TASKS):
-    print(f'Задача {i + 1}: {random.randint(1, SOL_CNT)}')
+CNT_QUESTION = 39
+
+for i in range(CNT):
+    print(f'Задача LeetCode {i + 1}: {random.randint(1, CNT_LEETCODE)}')
+print('')
+for j in range(CNT):
+    print(f'Номер вопроса {j + 1}: {random.randint(1, CNT_QUESTION)}')
